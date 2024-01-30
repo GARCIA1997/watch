20ac64922486af2c5852280e3f2a967ad37a62c5
Alejandro Garcia <agalvarez@concredito.com.mx>
2024-01-30 12:59:11 -0600

    Initial Commit

---
+++ miOficinaDemo Watch App/miOficinaDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  miOficinaDemoApp.swift
+//  miOficinaDemo Watch App
+//
+//  Created by Alejandro García on 30/01/24.
+//
+
+import SwiftUI
+
+@main
+struct miOficinaDemo_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
